9f2ad391486a2082790e6cd7b561cea0eb2bf596
jxcv0 <jmicoud02@gmail.com>
2022-05-26 19:36:39 +0100

    add basic maths

--- include/xen.h
+++ include/xen.h
@@ -4,6 +4,7 @@
 #include "glad.h"
 #include <GLFW/glfw3.h>
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -11,6 +12,79 @@
 static GLFWwindow* window;
 static float window_w;
 static float window_h;
+
+typedef struct vec2_t
+{
+    float values[2];
+} vec2_t;
+
+typedef struct vec3_t
+{
+    float values[3];
+} vec3_t;
+
+typedef struct vec4_t
+{
+    float values[4];
+} vec4_t;
+
+typedef struct mat4_t
+{
+    float values[4][4];
+} mat4_t;
+
+// TODO by value or ptr
+
+vec4_t row(mat4_t m, int n)
+{
+    vec4_t result = {
+        .values = {m.values[n][0], m.values[n][1], m.values[n][2], m.values[n][3]}
+    };
+    return result;
+}
+
+vec4_t col(mat4_t m, int n)
+{
+    vec4_t result = {
+        .values = {m.values[0][n], m.values[1][n], m.values[2][n], m.values[3][n]}
+    };
+    return result;
+}
+
+float dot_v4(vec4_t v1, vec4_t v2)
+{
+    return (v1.values[0] * v2.values[0])
+         + (v1.values[1] * v2.values[1])
+         + (v1.values[2] * v2.values[2])
+         + (v1.values[3] * v2.values[3]);
+}
+
+mat4_t cross_m4(mat4_t m1, mat4_t m2)
+{
+    mat4_t result;
+
+    result.values[0][0] = dot_v4(row(m1, 0), col(m2, 0));
+    result.values[0][1] = dot_v4(row(m1, 0), col(m2, 1));
+    result.values[0][2] = dot_v4(row(m1, 0), col(m2, 2));
+    result.values[0][3] = dot_v4(row(m1, 0), col(m2, 3));
+
+    result.values[1][0] = dot_v4(row(m1, 1), col(m2, 0));
+    result.values[1][1] = dot_v4(row(m1, 1), col(m2, 1));
+    result.values[1][2] = dot_v4(row(m1, 1), col(m2, 2));
+    result.values[1][3] = dot_v4(row(m1, 1), col(m2, 3));
+
+    result.values[2][0] = dot_v4(row(m1, 2), col(m2, 0));
+    result.values[2][1] = dot_v4(row(m1, 2), col(m2, 1));
+    result.values[2][2] = dot_v4(row(m1, 2), col(m2, 2));
+    result.values[2][3] = dot_v4(row(m1, 2), col(m2, 3));
+
+    result.values[3][0] = dot_v4(row(m1, 3), col(m2, 0));
+    result.values[3][1] = dot_v4(row(m1, 3), col(m2, 1));
+    result.values[3][2] = dot_v4(row(m1, 3), col(m2, 2));
+    result.values[3][3] = dot_v4(row(m1, 3), col(m2, 3));
+
+    return result;
+}
 
 void on_resize(GLFWwindow* window, int width, int height)
 {
@@ -89,7 +163,7 @@ void shader_check_link(GLuint prgm_id)
     glGetProgramiv(prgm_id, GL_LINK_STATUS, &success);
     if (!success)
     {
-        glGetProgramInfoLog(prgm_id, 1024, NULL, log)
+        glGetProgramInfoLog(prgm_id, 1024, NULL, log);
         printf("Shader program linking error: %s\n", log);
     }
 }
