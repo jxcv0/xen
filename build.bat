640a3b562e294087bbc314dcc221c4657e554214
Jacob Micoud <jmicoud02@gmail.com>
2022-06-06 10:38:26 +0100

    move bins to folder

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-gcc kv.c glad.c -I include/ -Wall -Werror -o run -O3 -ggdb -lpthread -lassimp lib\glfw3.dll
+gcc kv.c glad.c -I include/ -Wall -Werror -o bin\run -O3 -ggdb -lpthread -lassimp lib\glfw3.dll
