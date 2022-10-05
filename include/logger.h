908a1c1b5caacb1460402801f760e49ddaaedbd1
jmicoud <jacob.micoud@zenitel.com>
2022-10-05 17:27:39 +0100

    clang format

--- include/logger.h
+++ include/logger.h
@@ -1,20 +1,27 @@
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 
 #ifndef NDEBUG
-#define log_debug(...) printf("DEBUG --> "); printf(__VA_ARGS__)
+#define log_debug(...)  \
+  printf("DEBUG --> "); \
+  printf(__VA_ARGS__)
 #else
 #define log_debug(...)  // no-op
 #endif
 
-#define log_always(...) printf("INFO ---> "); printf(__VA_ARGS__)
+#define log_always(...) \
+  printf("INFO ---> "); \
+  printf(__VA_ARGS__)
 
-#define log_fatal(...) printf("FATAL --> "); printf(__VA_ARGS__); \
-  printf("*** errno: %s\n", strerror(errno)); exit(EXIT_FAILURE)
+#define log_fatal(...)                        \
+  printf("FATAL --> ");                       \
+  printf(__VA_ARGS__);                        \
+  printf("*** errno: %s\n", strerror(errno)); \
+  exit(EXIT_FAILURE)
 
 #endif  // LOGGER_H_
