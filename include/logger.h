825df5a9d700cce1112d662b34172efa3e6ffeb9
jmicoud <jacob.micoud@zenitel.com>
2022-10-05 17:12:01 +0100

    add other debug levels

--- include/logger.h
+++ include/logger.h
@@ -2,11 +2,19 @@
 #define LOGGER_H_
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 #ifndef NDEBUG
-#define log_debug(...) printf(__VA_ARGS__)
+#define log_debug(...) printf("DEBUG --> "); printf(__VA_ARGS__)
 #else
 #define log_debug(...)  // no-op
 #endif
+
+#define log_always(...) printf("INFO ---> "); printf(__VA_ARGS__)
+
+#define log_fatal(...) printf("FATAL --> "); printf(__VA_ARGS__); \
+  printf("*** errno: %s\n", strerror(errno)); exit(EXIT_FAILURE)
 
 #endif  // LOGGER_H_
--- test/logger_tests.c
+++ test/logger_tests.c
@@ -1,7 +1,14 @@
+// #define NDEBUG
 #include "logger.h"
 
 int main() {
+    log_always("This message will appear even in NDEBUG\n");
     log_debug("This is a debug message\n");
     log_debug("10: %d TEN: %s\n", 10, "ten");
+    int *var = NULL;
+    if (var == NULL) {
+        errno = ENOENT;
+        log_fatal("var is null\n");
+    }
     return 0;
 }
